2e66e7a00bfa4f70954f936bf6177c4ff4306890
Timoum <timoum@mailoo.org>
2021-07-01 16:46:59 +0200

    exam

---
+++ EXAMEN/JS/exercice01.js
@@ -0,0 +1,19 @@
+// affectation et recuperation des elements html
+const section = document.getElementById("P");
+
+const button = document.getElementById("btn");
+
+//Affectation de la propriété .hidden à la section et au bouton
+
+p.hidden = true;
+btn.hidden = false;
+
+//active la fonction on click sur le bouton pour apparition de la section 
+// disparition de la section.
+
+btn.onclick = function (){
+    if (p.hidden) p.hidden = false;
+    else btn.hidden = true;
+}
+
+
---
+++ EXAMEN/JS/exercice02.js
@@ -0,0 +1,6 @@
+//ecuperation menu option 
+const front = document.getElementById("#front");
+console.log('front :',fd);
+const back = document.getElementById(db);
+console.log('back :', db);
+
