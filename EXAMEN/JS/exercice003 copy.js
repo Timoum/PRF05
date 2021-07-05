ed8f1fa8997c478a5d14e6342389a64f2aff73df
timoum <timoum@mailoo.org>
2021-07-05 07:59:23 +0200

    fetch bootstrap memoflex  test jquery

--- EXAMEN/JS/exercice003 copy.js
+++ EXAMEN/JS/exercice003 copy.js
@@ -1,61 +1,46 @@
-//declaration est affectation de l'input formulaire
 const pnom = document.getElementById("in");
-
-// verification de la prise en charge de l'input
 console.log("nom :", pnom);
-
-//declaration est affectation du bouton de recherche
 const bouton = document.getElementById("btn");
-
-//declaration est affectation du bouton de recherche
 console.log("bouton :", bouton);
 
-//Declaration et affectation du clic sur le bouton de recherche
 bouton.onclick = function () {
   console.log(`choix prenom : `, pnom.value);
 
-  //==================================================================//
-
-  // récupération des données qui correspondent à la recherche prenom
+  // // récupération des données qui correspondent au département cliqué
   fetch(`https://api.nationalize.io/?name=${pnom.value}`)
     .then((data) => {
       console.log(`https://api.nationalize.io/?name=${pnom.value}`);
       // data est de type Response
       console.log(`data json : `);
-      //Vérification que la donnée soit bien au format json
+      // Je vérifie si la données est bien du json
       // via la fonction json qui renvoie une promesse
       return data.json();
     })
     .then(function (data) {
       // Data est de type tableau d'objets
       console.log(`dataprenom : `, data);
-      console.log(`datacodepays : `, data.country[0].country_id);
+      console.log(`datacodepays: `, data.country[0].country_id);
+      ///////////////////////////////////////////////////////////
 
-      //=========================================================================//
-
-      // récupération des données qui correspondent aux pays d'origine des prénnom
       fetch(
         `https://restcountries.eu/rest/v2/alpha/${data.country[0].country_id}`
       )
         .then((data) => {
           // data est de type Response
           console.log(`data json : `);
-          //Vérification que la donnée soit bien au format json
+          // Je vérifie si la données est bien du json
           // via la fonction json qui renvoie une promesse
           return data.json();
         })
         .then(function (data) {
           // Data est de type tableau d'objets
           console.log(`datapays: `, data);
-          console.log(`Pays d'origine : `, data.name);
+          console.log(`Pays : `, data.name);
 
-          //declaration et affectation ce la variable affichage
-          const display = data.name;
-          //recuperation des données et affichage HTML
-          document.querySelector("#data").innerHTML = display;
+          ///////////////////////////////////////////////////////////
+
+          //////////////////////////////////////////////////////////////
         })
-        //======================================================//
-
         .catch((error) => {
           console.error(`erreur : `, error.message);
         });
---
+++ Jquery/tests/js/testexamexo1.js
@@ -0,0 +1,11 @@
+jQuery(function ($) {
+  const button = $(`<button> En Savoir PLus</button>`).appendTo(
+    "body:last-child"
+  );
+  const section = section.hide;
+  h1.hide();
+
+  button.click(function () {
+    h1.toggle();
+  });
+});
